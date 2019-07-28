7252d2211ba69d3fa1f950862cdcaef7cea0ea5f
RafaelSouza94 <rafaelrs57@hotmail.com>
2019-07-28 12:12:41 -0700

    New logging file

---
+++ Assert_Test/logs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.debug('Start of program')
+
+variable = "var"
+
+# print the variable with logging
+logging.debug("Variable value: {}".format(variable))
+print("Program doing regular stuff...")
+print("Now its over...")
+
+logging.debug("End of program")
